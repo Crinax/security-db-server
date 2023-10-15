4241536e374a63ee318458e8ac1c25c0ab164656
Kirill Sedyshev <agcrinax@gmail.com>
2023-10-15 12:16:42 +0300

    feat(diesel_migration): add diesel_migration create

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod api;
 
+use diesel_migrations::{embed_migrations, EmbeddedMigrations};
 use actix_web::{HttpServer, App};
+use dotenvy::dotenv;
+use std::env;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-   HttpServer::new(|| {
-       App::new()
-           .service(api::make_service())
-   }) 
-   .bind(("127.0.0.1", 7878))?
-   .run()
-   .await
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // connection.run_pending_migrations(MIGRATIONS)?;
+
+    HttpServer::new(|| {
+        App::new()
+            .service(api::make_service())
+    }) 
+    .bind(("127.0.0.1", 7878))?
+    .run()
+    .await
 }
