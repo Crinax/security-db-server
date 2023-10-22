b3bfe2a78ef5582f7dddc1b70e31a0382c513337
Kirill Sedyshev <agcrinax@gmail.com>
2023-10-22 07:50:28 +0300

    feat(main): add logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod api;
 
 use diesel_migrations::{embed_migrations, EmbeddedMigrations};
-use actix_web::{HttpServer, App};
+use actix_web::{HttpServer, App, middleware::Logger};
 use dotenvy::dotenv;
 use std::env;
+use env_logger::Env;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
@@ -15,8 +16,11 @@ async fn main() -> std::io::Result<()> {
 
     // connection.run_pending_migrations(MIGRATIONS)?;
 
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(api::make_service())
     }) 
     .bind(("127.0.0.1", 7878))?
